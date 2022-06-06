af7122070b4c7f3f72993f03c95fd6accf4d8ac6
Maicol <michael.negrete16@gmail.com>
2022-06-06 15:21:39 -0500

    Recuperar informacion de un posteo

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const app = express()
 // Directorio publico
 app.use(express.static('public'))
 
+// Lectura y parceo del body
+app.use(express.json())
+
 // Rutas
 app.use('/api/auth', require('./routes/auth'))
 // TODO: CRUD: eventos
