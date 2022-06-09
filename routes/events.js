95c08fc1440a5f81fbe26f4b056a1bccfc0584cc
Maicol <michael.negrete16@gmail.com>
2022-06-09 00:02:32 -0500

    Validar campos en las fechas

--- routes/events.js
+++ routes/events.js
@@ -1,7 +1,9 @@
 const {Router} = require('express')
 const {getEventos,crearEvento,actualizarEvento,eliminarEvento} = require('../controllers/events')
 const {validarJWT} = require('../middlewares/validar-jwt')
-
+const {check} = require('express-validator')
+const {validarCampos} = require('../middlewares/validar-campos')
+const {isDate} = require('../helpers/isDate')
 
 const router = Router()
 
@@ -10,7 +12,12 @@ const router = Router()
 router.get('/',validarJWT, getEventos)
 
 // Crear Eventos
-router.post('/',validarJWT, crearEvento)
+router.post('/',[
+    check('title','El titulo es obligatorio').not().isEmpty(),
+    check('startDate','La fecha de inicio es obligatorio').custom(isDate),
+    check('endDate','La fecha de finalizacion es obligatorio').custom(isDate),
+    validarCampos,validarJWT
+], crearEvento)
 
 // Actualizar evento
 router.put('/:id',validarJWT, actualizarEvento)
