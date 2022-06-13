76e876204205c12b096143e7c638f51dd7d2bd3a
Maicol <michael.negrete16@gmail.com>
2022-06-13 08:58:25 -0500

    Eliminar evento

--- controllers/events.js
+++ controllers/events.js
@@ -50,7 +50,7 @@ const actualizarEvento  = async (req,res = response) => {
         // Consultar que el id sea valido
         const evento = await Evento.findById(eventoId)
         if(!evento){
-            res.status(404).json({
+            return res.status(404).json({
                 ok:false,
                 msg:"Evento no existe por ese ID"
             })
@@ -87,11 +87,43 @@ const actualizarEvento  = async (req,res = response) => {
 
 }
 
-const eliminarEvento = (req,res = response) => {
-    res.status(200).json({
-        ok:true,
-        msg:'Eliminar evento'
-    })
+const eliminarEvento = async (req,res = response) => {
+
+    const eventoId = req.params.id
+    const uid = req.uid
+
+    try {
+
+        const evento = await Evento.findById(eventoId)
+        if(!evento){
+            return res.status(404).json({
+                ok: false,
+                msg:'Evento no existe por ese ID'
+            })
+        }
+
+        if(evento.user.toString() !== uid){
+            return res.status(401).json({
+                ok:false,
+                msg:'No tiene privilegios para eliminar este evento'
+            })
+        }
+
+        await Evento.findByIdAndDelete(eventoId)
+
+        res.json({
+            ok:true,
+            msg:'Evento eliminado con exito'
+        })
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            ok:false,
+            msg:'Hable con un administrador'
+        })
+    }
+
 }
 
 
